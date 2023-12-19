75de6bdb2e7d24dc62f01fcc135b6a41ccc980f5
RobertoDev3 <roberto.dev3@hotmail.com>
2023-12-19 12:52:49 -0300

    🎨 Adicionado projetos

--- logic.js
+++ logic.js
@@ -1,18 +1,3 @@
-function getDogImages() {
-  const images = document.querySelectorAll(".card-img");
-  images.forEach((image) => {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then((response) => response.json())
-      .then((data) => {
-        image.src = data.message;
-      })
-      .catch((error) =>
-        console.error("Ocorreu um erro ao buscar a imagem:", error)
-      );
-  });
-}
-getDogImages();
-
 let homeHeight = document.getElementById("home").offsetHeight;
 document.getElementById("particles-container").style.height = homeHeight + "px";
 
